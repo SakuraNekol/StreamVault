710819729858cd30987594e3c4de35ae12277626
MochiMoon <39724019+lemon8866@users.noreply.github.com>
2025-08-22 21:45:44 +0800

    refactor: optimize deprecated code patterns

--- backstage/src/main/java/com/flower/spirit/SpiritApplication.java
+++ backstage/src/main/java/com/flower/spirit/SpiritApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.flower.spirit.utils.FileUtil;
@@ -15,6 +16,7 @@ import com.flower.spirit.utils.FileUtil;
 @EnableScheduling
 @EnableJpaRepositories(basePackages = "com.flower.spirit.dao")
 @EntityScan(basePackages = "com.flower.spirit.entity")
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class SpiritApplication {
 
 	public static void main(String[] args) {
